e6d43160c1b7508767f3be77b3eecfb1e14a04a6
zjohnsonedtech <zac@z-johnson.com>
2016-05-23 13:13:56 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
 // fixes navbar when scrolled to top and jumbotron out of view
 document.onscroll = function() {
     if( $(window).scrollTop() > $('jumbotron').height() ) {
-        $('navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+        $('nav > div.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
     }
     else {
-        $('navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+        $('nav > div.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
     }
 };
